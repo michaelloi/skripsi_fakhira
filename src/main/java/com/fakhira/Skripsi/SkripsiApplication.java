b14260b147bacc80966a1b03ab822fc51ad74b65
michaelloi <loimichael0@gmail.com>
2022-04-05 05:33:09 +0700

    fe

---
+++ src/main/java/com/fakhira/Skripsi/SkripsiApplication.java
@@ -0,0 +1,13 @@
+package com.fakhira.Skripsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkripsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkripsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fakhira/Skripsi/SkripsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakhira.Skripsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkripsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
